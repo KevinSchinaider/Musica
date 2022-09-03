4dd3b6a27d02e0effba6d4dd9f141851b909e1b4
KevinSchinaider <kevinschinaider05@outlook.com>
2022-09-02 22:37:52 -0300

    Solução música

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("O cara que pensa em você toda hora");
+Console.WriteLine("Que conta os segundos se você demora");
+Console.WriteLine("Que está todo o tempo querendo te ver");
+Console.WriteLine("Porque já não sabe ficar sem você");
+Console.WriteLine("E no meio da noite te chama");
+Console.WriteLine("Pra dizer que te ama");
+Console.WriteLine("Esse cara sou eu");
